910da0f8e4f1077803a12bc414d48acaa2641db6
jin-sung-oh <wlsqoren@naver.com>
2025-06-26 10:47:10 +0900

    refactor:busker search 수정 끝 (#8)

--- src/main/java/back/vybz/search_service/busker/presentation/BuskerSearchController.java
+++ src/main/java/back/vybz/search_service/busker/presentation/BuskerSearchController.java
@@ -9,13 +9,12 @@ import back.vybz.search_service.common.entity.BaseResponseEntity;
 import back.vybz.search_service.common.util.CursorPage;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/search")
 @RequiredArgsConstructor
@@ -31,6 +30,11 @@ public class BuskerSearchController {
     @GetMapping("/buskers")
     public BaseResponseEntity<CursorPage<ResponseScrollSearchBuskerVo>> searchBuskers(@ModelAttribute RequestScrollSearchBuskerVo requestScrollSearchBuskerVo) throws IOException {
 
+        log.debug("🔍 [검색 요청] keyword='{}', size={}, cursorFollowerCount={}, cursorBuskerUuid='{}'",
+                requestScrollSearchBuskerVo.getKeyword(),
+                requestScrollSearchBuskerVo.getSize(),
+                requestScrollSearchBuskerVo.getCursorFollowerCount(),
+                requestScrollSearchBuskerVo.getCursorBuskerUuid());
         return new BaseResponseEntity<>(
                 buskerSearchService.searchBuskers(RequestScrollSearchBuskerDto.from(requestScrollSearchBuskerVo))
                         .map(ResponseScrollSearchBuskerDto::toVo)
